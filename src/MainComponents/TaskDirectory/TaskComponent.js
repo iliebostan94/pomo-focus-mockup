bd5e52fb13734661a6b9816a3608ec88f36fda57
ilie <ilie@smartdata.solutions>
2022-08-07 16:33:30 +0300

    MUI button set next to Tasks title.

--- src/MainComponents/TaskDirectory/TaskComponent.js
+++ src/MainComponents/TaskDirectory/TaskComponent.js
@@ -7,14 +7,64 @@ import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 
+const options = [
+    'Atria',
+    'Callisto',
+    'Dione',
+    'Ganymede',
+  ];
+const ITEM_HEIGHT = 48;
+
 const TaskComponent = () => {
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
+
     return (
         <div className='taskWrapper'>
-            <div>
-                <p>Tasks</p>
-
+            <div className='tasksUpperBar'>
+                <p className='tasksUpperBar-title'>Tasks</p>
+                <IconButton
+                    aria-label="more"
+                    id="long-button"
+                    aria-controls={open ? 'long-menu' : undefined}
+                    aria-expanded={open ? 'true' : undefined}
+                    aria-haspopup="true"
+                    onClick={handleClick}
+                >
+                <MoreVertIcon />
+                </IconButton>
+                <Menu
+                    id="long-menu"
+                    MenuListProps={{
+                    'aria-labelledby': 'long-button',
+                    }}
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={handleClose}
+                    PaperProps={{
+                    style: {
+                        maxHeight: ITEM_HEIGHT * 4.5,
+                        width: '20ch',
+                    },
+                    }}
+                >
+                    {options.map((option) => (
+                <MenuItem key={option} selected={option === 'Atria'} onClick={handleClose}>
+                    {option}
+                </MenuItem>
+                    ))}
+                </Menu>
             </div>
-            <CreateTaskForm />
+
+
+                <CreateTaskForm />
         </div>
     )
 }
